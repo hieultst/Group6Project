8e73a0b51c24a4e12f5974deaee89859e3b4a0c6
trunghieu <infor.letrunghieu@gmail.com>
2024-05-21 17:34:31 +0700

    Initial Commit

---
+++ Group6Project/Group6ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group6ProjectApp.swift
+//  Group6Project
+//
+//  Created by Trung Hieu on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Group6ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
