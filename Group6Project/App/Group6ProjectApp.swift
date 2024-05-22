c9e5b4dd54e0aa92f51c6740d6984a4c576ed712
trunghieu <infor.letrunghieu@gmail.com>
2024-05-22 12:39:13 +0700

    LoginView and RegistrationView

--- Group6Project/App/Group6ProjectApp.swift
+++ Group6Project/App/Group6ProjectApp.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Group6ProjectApp: App {
+    @StateObject var viewModel = AuthViewModel()
+    
+    init() {
+        FirebaseApp.configure()
+    }
+
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(viewModel)
         }
     }
 }
---
+++ Group6Project/Model/User.swift
@@ -0,0 +1,28 @@
+//
+//  User.swift
+//  Group6Project
+//
+//  Created by Trung Hieu on 21/05/2024.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    let fullname: String
+    let email: String
+    
+    var initials: String {
+        let formatter = PersonNameComponentsFormatter()
+        if let components = formatter.personNameComponents(from: fullname) {
+            formatter.style = .abbreviated
+            return formatter.string(from: components)
+        }
+        
+        return ""
+    }
+}
+
+extension User {
+    static var MOCK_USER = User(id: NSUUID().uuidString, fullname: "Le Trung Hieu", email: "test@gmail.com")
+}
